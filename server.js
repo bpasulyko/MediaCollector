1afaf12f899ff0e2474c8d6cae9b285a6161f4e9
bpasulyko <bpasulyko@gmail.com>
2016-06-03 16:00:40 -0600

    Server setup
    Set up the server.js and added the necessary dependencies

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('This is a test');
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var db;
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
