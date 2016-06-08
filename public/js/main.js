da35a87ea3a0f446fac084252cc4d071a82cd2cd
Brady <bpasulyko@gmail.com>
2016-06-07 20:03:47 -0600

    Added icons to left nav and page title

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 var addItemDropdown;
+var $pageTitle;
 
 $(document).ready(function() {
   setupPageView();
@@ -6,6 +7,7 @@ $(document).ready(function() {
 });
 
 function setupPageView() {
+  $pageTitle = $('.brand-logo');
   var page = window.location.href.split('/').pop();
   if (page === 'movies') {
     initializeMoviePageView();
@@ -19,18 +21,22 @@ function setupPageView() {
 }
 
 function initializeMoviePageView() {
+  $pageTitle.html('<i class="material-icons left">theaters</i>Movies');
   $('#main').append('<h1>MOVIES</h1>');
 }
 
 function initializeTvShowPageView() {
+  $pageTitle.html('<i class="material-icons left">airplay</i>TV Shows');
   $('#main').append('<h1>TV</h1>');
 }
 
 function initializeGamePageView() {
+  $pageTitle.html('<i class="material-icons left">games</i>Games');
   $('#main').append('<h1>GAMES</h1>');
 }
 
 function initializeHomePageView() {
+  $pageTitle.html('Media Collector');
   $('#main').append('<h1>HOME</h1>');
   initializePageComponents();
 }
